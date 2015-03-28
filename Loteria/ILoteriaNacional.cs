750258ca0bb76e5d28898a11d913f6c276647f7d
ejimenezdelgado <ejimenezdelgado@hotmail.com>
2015-03-28 14:06:48 -0600

    Clase 22

--- Loteria/ILoteriaNacional.cs
+++ Loteria/ILoteriaNacional.cs
@@ -6,13 +6,14 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using AccesoDatos;
 
 namespace Loteria
 {
     [ServiceContract]
     public interface ILoteriaNacional
     {
-        [OperationContract]
+/*        [OperationContract]
         int NumeroGanador();
 
         [OperationContract]
@@ -22,7 +23,28 @@ namespace Loteria
         void AgregarPremio(Premio premio);
 
         [OperationContract]
-        double TipoCambio();
+        double TipoCambio();*/
+
+        [OperationContract]
+        void AgregarSorteo(sorteo sorteo);
+
+        [OperationContract]
+        void EditarSorteo(sorteo sorteo);
+
+        [OperationContract]
+        void EliminarSorteo(sorteo sorteo);
+
+        [OperationContract]
+        void AgregarPremio(premio premio);
+
+        [OperationContract]
+        void EditarPremio(premio premio);
+
+        [OperationContract]
+        void EliminarPremio(premio premio);
+
+        [OperationContract]
+        bool AgregarUsuario(usuario usuario);
 
     }
 }
--- Loteria/LoteriaNacional.svc.cs
+++ Loteria/LoteriaNacional.svc.cs
@@ -1,6 +1,8 @@
-﻿using Loteria.tipoCambio;
+﻿using AccesoDatos;
+using Loteria.tipoCambio;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -11,7 +13,7 @@ namespace Loteria
 {
     public class LoteriaNacional : ILoteriaNacional
     {
-        public int NumeroGanador()
+     /*   public int NumeroGanador()
         {
             Random oRandom=new Random();
             return oRandom.Next();
@@ -42,6 +44,59 @@ namespace Loteria
             var tipoCambio = oTipoCambio.ObtenerIndicadoresEconomicos("318", "14/03/2015", "14/03/2015", "UTN", "N");
 
             return Convert.ToDouble(tipoCambio.Tables[0].Rows[0]["NUM_VALOR"].ToString());
+        }*/
+
+        public void AgregarSorteo(sorteo sorteo,usuario usuario)
+        {
+            ModeloLoteriaContainer modeloLoteria = new ModeloLoteriaContainer();
+            modeloLoteria.sorteo.Add(sorteo);
+            modeloLoteria.SaveChanges();            
+        }
+
+        public void EditarSorteo(sorteo sorteo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EliminarSorteo(sorteo sorteo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AgregarPremio(premio premio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditarPremio(premio premio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EliminarPremio(premio premio)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public bool AgregarUsuario(usuario usuario)
+        {
+            try
+            {
+                ModeloLoteriaContainer modeloLoteria = new ModeloLoteriaContainer();
+                var md5 = modeloLoteria.Database.SqlQuery<string>("select convert(varchar(32),HashBytes('MD5','@password'),2)",
+                     new object[] { usuario.password }).First();
+
+                usuario.password = md5;
+
+                modeloLoteria.usuario.Add(usuario);
+                modeloLoteria.SaveChanges();
+
+            }catch(Exception error)
+            {
+                return false;
+            }
+            return true;           
         }
     }
 }
